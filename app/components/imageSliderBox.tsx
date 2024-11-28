f9d172e32cfc72f0221f67175535db7df3e8f405
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-11-28 22:56:37 +0000

    cosmetics arrows

--- app/components/imageSliderBox.tsx
+++ app/components/imageSliderBox.tsx
@@ -10,15 +10,15 @@ const IMAGES = [img1, img2, img3, img4, img5]
 
 const ImageSliderBox = () => {
   return (
-    <section className="flex_center h-screen  w-full snap-start bg_photos flex-col ">
+    <section className="flex_center h-screen  snap-start bg_photos flex-col ">
       {/* <a name="photos"></a> */}
       {/* <a href="#price"><UpPulse /></a> */}
-      <div className="bg-black p-1 mt-28 rounded-md">
-        <Up3 fill="black" stroke="white" className="text-4xl animate-bounce " />
+      <div className="bg-black p-1 mt-1 rounded-md animate-bounce">
+        <Up3 fill="black" stroke="white" className="text-4xl  " />
       </div>
       <ImageSlider imageUrls={IMAGES} />
-      <div className="mb-24">
-        <Down3 fill="black" stroke="white" className="text-4xl animate-bounce" />
+      <div className="mb-20 animate-bounce">
+        <Down3 fill="black" stroke="white" className="text-4xl" />
       </div>
 
       {/* <Navbar /> */}
