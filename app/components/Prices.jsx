f2535c23d49fbfef991f160fc344000b9269f6a6
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-01 16:55:10 +0000

    get prices with Prisma

--- app/components/Prices.jsx
+++ app/components/Prices.jsx
@@ -1,19 +1,13 @@
-async function getPrices() {
-  const res = await fetch('https://oskarsbarbers4men.co.uk/indexAPI.php')
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
+import prisma from '@/prisma/client'
 
-  return res.json()
-}
 const Prices = async () => {
-  const prices = await getPrices()
+  const prices = await prisma.prices.findMany()
 
   return (
     <div className="w-full sm:w-[400px] bg-indigo-800 opacity-85 p-3 rounded-md">
       <ul className="flex flex-col items-center text-white text-xl">
         {prices.map(price => (
-          <li key={price.price_id}>
+          <li key={price.id}>
             {price.item} <span>&pound; {price.price}</span>
           </li>
         ))}
