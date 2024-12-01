b8f3e452ef6f1c2a165a3e15fdd3183b18c8fcdc
Nazie Sharid <nazie.sh4rid@gmail.com>
2024-12-01 18:47:34 +0000

    get hours using Prisma

--- app/components/Hours.jsx
+++ app/components/Hours.jsx
@@ -1,3 +1,5 @@
+import prisma from '@/prisma/client'
+
 async function getHours() {
   const res = await fetch('https://oskarsbarbers4men.co.uk/indexAPI_hours.php')
   if (!res.ok) {
@@ -8,8 +10,22 @@ async function getHours() {
   return res.json()
 }
 const OpeningHours = async () => {
-  const hours = await getHours()
+  // const hours = await getHours()
+  const hours = await prisma.hours.findMany()
 
+  const map = {
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6,
+    Sunday: 7,
+  }
+
+  const hours2 = hours.sort((a, b) => {
+    return map[a.day] - map[b.day]
+  })
   return (
     <>
       <div
@@ -18,26 +34,38 @@ const OpeningHours = async () => {
         itemScope
         itemType="http://schema.org/openingHours"
       >
-        {hours.map((hour, index) => (
-          <time
-            key={index}
-            className="flex text-white text-xl"
-            itemProp="openingHours"
-            dateTime={
-              hour.day_code +
-              ' ' +
-              hour.start.slice(0, 2) +
-              ':' +
-              hour.start.slice(2) +
-              '-' +
-              hour.end.slice(0, 2) +
-              ':' +
-              hour.end.slice(2)
-            }
-          >
-            {hour.day} {hour.start} to {hour.end}
-          </time>
-        ))}
+        {hours2.map((hour, index) => {
+          const startFormatted =
+            JSON.stringify(hour.start).length === 3
+              ? '0' + JSON.stringify(hour.start)
+              : JSON.stringify(hour.start)
+          const endFormatted =
+            JSON.stringify(hour.end).length === 3
+              ? '0' + JSON.stringify(hour.end)
+              : JSON.stringify(hour.end)
+
+          return (
+            <time
+              key={index}
+              className="flex text-white text-xl"
+              itemProp="openingHours"
+              dateTime={
+                hour.day_code +
+                ' ' +
+                startFormatted.slice(0, 2) +
+                ':' +
+                startFormatted.slice(2) +
+                '-' +
+                endFormatted.slice(0, 2) +
+                ':' +
+                endFormatted.slice(2)
+              }
+            >
+              {hour.day} {startFormatted} to {endFormatted}
+            </time>
+          )
+        })}
+        {JSON.stringify(hours)}
       </div>
     </>
   )
